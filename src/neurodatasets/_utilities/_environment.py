ad3fdd40fc30b2cfaeca6beef4505e6683cad86c
Raj Magesh Gauthaman <git@raj-magesh.org>
2026-06-22 17:30:21 -0400

    remove xdg_base_dirs dependency

--- src/neurodatasets/_utilities/_environment.py
+++ src/neurodatasets/_utilities/_environment.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
 
-from xdg_base_dirs import xdg_data_home
-
-NEURODATASETS_HOME = Path(
-    os.getenv(
-        "NEURODATASETS_HOME",
-        str(xdg_data_home() / ".cache" / "neurodatasets"),
-    ),
-)
+if neurodatasets_home := os.getenv("NEURODATASETS_HOME"):
+    NEURODATASETS_HOME = Path(neurodatasets_home)
+elif xdg_data_home := os.getenv("XDG_DATA_HOME"):
+    NEURODATASETS_HOME = Path(xdg_data_home) / "neurodatasets"
+else:
+    NEURODATASETS_HOME = Path.home() / ".local" / "share" / "neurodatasets"
